02ea62dd35462d3f9dc27e3195caf024468eeb95
yahweh <0505yahweh@gmail.com>
2018-08-01 05:38:30 +0800

    7.31

---
+++ webcam.py
@@ -0,0 +1,32 @@
+"""
+Simply display the contents of the webcam with optional mirroring using OpenCV 
+via the new Pythonic cv2 interface.  Press <esc> to quit.
+"""
+
+import cv2
+import os
+import matplotlib
+
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(1)
+    while not cam.isOpened():
+        cam.open('/dev/bus/usb/001/005')
+    while True:
+        ret, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+    cv2.destroyAllWindows()
+    cam.release()
+
+def plot_cv():
+    pass
+
+def main():
+    show_webcam()
+
+
+if __name__ == '__main__':
+    main()
